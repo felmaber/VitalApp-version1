e918ee744ad8c0df89941dfd6db02d4c244e0f5c
julieth <ljosoriot@gmail.com>
2017-06-08 19:25:02 -0500

    version junio 8 2017 hora: 7:24pm julieth

--- app/src/main/java/juliethosorio/vitalapp/PerfilActivity.java
+++ app/src/main/java/juliethosorio/vitalapp/PerfilActivity.java
@@ -16,6 +16,7 @@ public class PerfilActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         //*
+        /**/
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
