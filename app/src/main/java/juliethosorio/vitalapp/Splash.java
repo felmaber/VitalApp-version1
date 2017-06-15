fa09b7f35f9846bc340f5e0c10abd2dcfae415a8
Felmaber <felmaber.garzon@gmail.com>
2017-06-14 21:56:46 -0500

    prueba commit

--- app/src/main/java/juliethosorio/vitalapp/Splash.java
+++ app/src/main/java/juliethosorio/vitalapp/Splash.java
@@ -6,7 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Splash extends AppCompatActivity {
-//hola
+//hola hola
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
